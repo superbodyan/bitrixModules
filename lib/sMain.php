f064983a54ee284f3d1cb649de77c114bde31497
superbodyan <valevich007@gmail.com>
2020-10-22 00:52:08 +0300

    added method makeUrlRewriteRule

--- lib/sMain.php
+++ lib/sMain.php
@@ -2,6 +2,7 @@
 
 namespace superBIT;
 
+use Bitrix\Main\ArgumentNullException;
 use Bitrix\Main\Loader;
 
 abstract class sMain
@@ -10,5 +11,23 @@ abstract class sMain
     {
         if (!Loader::includeModule("superbit"))
             throw new \Bitrix\Main\LoaderException("Модуль superBIT не установлен");
+    }
+
+    public static function makeUrlRewriteRules()
+    {
+        // for RestApi ORM
+
+        $urlRew = array(
+            "CONDITION" => "#^/api/rest-api/([0-9]+)/#",
+            "RULE" => "ID=$1",
+            "ID" => "",
+            "PATH" => "/api/rest-api/index.php"
+        );
+
+        try {
+            \Bitrix\Main\UrlRewriter::add(SITE_ID, $urlRew);
+        } catch (ArgumentNullException $e) {
+            return $e->getMessage();
+        }
     }
 }
