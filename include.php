32d8555c4135088087cb4a3a5cf16294f34ccf98
superbodyan <valevich007@gmail.com>
2020-10-23 01:49:43 +0300

    update autoload file

--- include.php
+++ include.php
@@ -4,4 +4,8 @@ Bitrix\Main\Loader::registerAutoLoadClasses(null, [
     'superBIT\ORMTEST\OrmTestTable' => '/local/modules/superBIT/lib/OrmTestTable.php',
     'superBIT\sMain' => '/local/modules/superBIT/lib/sMain.php',
     'superBIT\RestApi' => '/local/modules/superBIT/lib/RestApi.php',
+    'superBIT\RegRu' => '/local/modules/superBIT/lib/RegRu.php',
+    'superBIT\cUrl' => '/local/modules/superBIT/lib/cUrl.php',
+    'superBIT\OrmDefault' => '/local/modules/superBIT/lib/OrmDefault.php',
+    'superBIT\RegRuOrmTable' => '/local/modules/superBIT/lib/RegRuOrmTable.php',
 ]);
